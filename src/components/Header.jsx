a2dd90ed89222614e78208ff396c673d2f425249
Mukeshzz <mukesh7.251097@gmail.com>
2024-12-19 20:43:19 +0530

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-[#0A2647] text-white p-4 ">
+    <header className="bg-[#0A2647] text-white p-4 fixed w-full">
       <nav className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Portfolio</h1>
         <ul className="flex space-x-6">
